911bfbd1f870ed36dc316121dcea81d620040e9c
JoshR <joshuarizek@gmail.com>
2020-07-21 10:51:49 -0400

    changes made to back-end

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -4,9 +4,12 @@ import '../css/Head.css'
 
 
 export default function Header(props) {
+
+
     return (
         <div className="heading">
-            <h1 className="Logo">Jot-It</h1>
+            {props.currentUser ? <Link to='/entries-list'><h1 className="Logo">Jot-It</h1></Link> : <Link to='/'><h1 className="Logo">Jot-It</h1></Link>}
+            {/* // <h1 className="Logo">Jot-It</h1> */}
             {props.currentUser ?
                 <div className="loggedinNav">
                     <p className="navText" ><a onClick={props.handleLogout}>Log out</a>
