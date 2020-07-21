c308b870fe35eaa13275fb9663c6e4a661830cca
JoshR <joshuarizek@gmail.com>
2020-07-21 11:35:00 -0400

    api fix

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NOD_ENV === 'production' ? 'https://jot-it-api.herokuapp.com' :'http://localhost:3000'
+const baseUrl = process.env.NOD_ENV === 'production' ? 'https://jot-it-api.herokuapp.com/' :'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
